d2a16f3d4d7ef8670e65a157b1dcde1c078af578
jangelsb <jangelsb.vcs@gmail.com>
2024-11-09 17:06:49 -0800

    * Add the start of unit tests
    Run: python3 -m unittest discover -s . -p "test_*.py"

---
+++ test_tokenize.py
@@ -0,0 +1,19 @@
+import unittest
+from git_filtering_internal import tokenize, TokenizationResult, Location, Command
+
+class TestTokenization(unittest.TestCase):
+    def test_nested_commands(self):
+        subcommands2 = [Command(title="level 3", action="")]
+        subcommands1 = [Command(title="level 2", action="", subcommands=subcommands2)]
+        commands = [Command(title="subcommands", action="", subcommands=subcommands1)]
+        locations = [Location(title="timer", directory="."), Location(title="calc", directory=".")]
+
+        result = tokenize("timer subcommands level 2 level", locations, commands)
+        self.assertEqual(result.location.title, "timer")
+        self.assertEqual(len(result.commands), 2)
+        self.assertEqual(result.commands[0].title, "subcommands")
+        self.assertEqual(result.commands[1].title, "level 2")
+        self.assertEqual(result.unfinished_query, "level")
+
+if __name__ == '__main__':
+    unittest.main()
