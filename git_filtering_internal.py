8ab34941af77083b35a90e1762a4529fabfb160a
jangelsb <jangelsb.vcs@gmail.com>
2024-11-16 22:50:36 -0800

    * Add suppport for actions to be set in the location config, per location

--- git_filtering_internal.py
+++ git_filtering_internal.py
@@ -86,9 +86,10 @@ class ResultItem:
         return item_dict
 
 class Location:
-    def __init__(self, title, directory):
+    def __init__(self, title, directory, actions_path=None):
         self.title = title
         self.directory = directory
+        self.actions_path = actions_path
 
 class Command:
     def __init__(self, title, action, secondaryAction=None, subtitle=None, command_type=CommandType.SINGLE_ACTION, icon_path=None, mods=None, values=None, values_command=None, subcommands=None, values_icon=None, subtitle_command=None, should_use_values_as_inline_commands=False):
@@ -379,15 +380,23 @@ def create_inline_commands(cmd):
 
 def generate_locations_from_yaml(yaml_string):
     def location_entry_processor(entry):
-        path = entry['path']
-        # Split the path into parts and process parts starting with '$'
-        path_parts = path.split('/')
-        for i, part in enumerate(path_parts):
-            if part.startswith('$'):
-                env_var_name = part[1:]
-                path_parts[i] = os.environ.get(env_var_name, part)
-        path = '/'.join(path_parts)
-        return Location(title=entry['title'], directory=path)
+        def process_path(path):
+            # Split the path into parts and process parts starting with '$'
+            path_parts = path.split('/')
+            for i, part in enumerate(path_parts):
+                if part.startswith('$'):
+                    env_var_name = part[1:]
+                    path_parts[i] = os.environ.get(env_var_name, part)
+            return '/'.join(path_parts)
+
+        title = entry['title']
+        path = process_path(entry['path'])
+        actions_path = entry.get('actions_path', None)
+
+        if actions_path:
+            actions_path = process_path(actions_path)
+
+        return Location(title=title, directory=path, actions_path=actions_path)
 
     yaml_data = yaml.safe_load(yaml_string)
     return [location_entry_processor(entry) for entry in yaml_data]
@@ -596,6 +605,9 @@ def main():
         # initial row of inline values
         for cmd in commands:
             commands.extend(create_inline_commands(cmd))
+
+        if alfred_input.location.actions_path:
+            commands.extend(create_commands_from_config(alfred_input.location.actions_path))
 
         process_commands_recursively(query_input=query_input, locations=locations, commands=commands)
         num_cmds = len(alfred_input.commands)
